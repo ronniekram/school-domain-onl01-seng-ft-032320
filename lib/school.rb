a1cf6e6cb4cae728b2aa63f81223b68be7ea886f
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-29 03:14:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School 
+  attr_accessor :roster, :student, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = roster
+    @roster = {}
+  end 
+  
+  def add_student(student, grade)
+    if roster.has_key?(grade)
+      roster[grade] << student 
+    else 
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end 
+  
+  def sort 
+    sort_hash = {}
+    roster.each do |k, v| 
+      sort_hash[k] = v.sort 
+    end 
+      sort_hash
+  end 
+  
+end
